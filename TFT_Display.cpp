740d531e7f3fcd63f5d3d832507b0ba27be91dd7
amenaruya <132125305+amenaruya@users.noreply.github.com>
2024-04-13 00:53:44 +0900

    Update TFT_Display.cpp

--- TFT_Display.cpp
+++ TFT_Display.cpp
@@ -1,30 +1,31 @@
 #include "TFT_Display.h"
 #include "enumerates.h"
 
-Displayer::Displayer(TFT_eSPI* const pTftLcd) {
+Displayer::Displayer(TFT_eSPI* const pTftLcd):
+pTft(pTftLcd) {
     /* TFT LCDの準備 */
-    pTftLcd -> begin();
-    pTftLcd -> setRotation(3);
-    pTftLcd -> setTextSize(3);
-    pTftLcd -> setTextColor(TFT_KUROBENI);
+    (this -> pTft) -> begin();
+    (this -> pTft) -> setRotation(3);
+    (this -> pTft) -> setTextSize(3);
+    (this -> pTft) -> setTextColor(TFT_KUROBENI);
     /* トップ画面を表示する */
-    this -> privateFShowMainScreen(pTftLcd);
+    this -> privateFShowMainScreen();
 }
 // Displayer::~Displayer() {}
 
-void Displayer::privateFShowMainScreen(TFT_eSPI* const pTftLcd) {
+void Displayer::privateFShowMainScreen() {
     /* 背景初期化 */
-    pTftLcd -> fillScreen(TFT_TOKINEZU);
+    (this -> pTft) -> fillScreen(TFT_TOKINEZU);
     /* ボタン指示 */
-    pTftLcd -> drawString("A", 166, 2);
-    pTftLcd -> drawString("B", 82, 2);
+    (this -> pTft) -> drawString("A", 166, 2);
+    (this -> pTft) -> drawString("B", 82, 2);
     /* メッセージ */
-    pTftLcd -> drawString("Push button", 60, 110);
-    pTftLcd -> drawString("A or B", 110, 135);
+    (this -> pTft) -> drawString("Push button", 60, 110);
+    (this -> pTft) -> drawString("A or B", 110, 135);
     this -> privateFChangeDisplayState(MAIN_SCREEN);
 }
 
-void Displayer::privateFChangeScreen(const DisplayState newState, TFT_eSPI* const pTftLcd) {
+void Displayer::privateFChangeScreen(const DisplayState newState) {
     if (this -> currentDisplayState != newState) {
         char* message;
         switch (newState) {
@@ -40,10 +41,10 @@ void Displayer::privateFChangeScreen(const DisplayState newState, TFT_eSPI* cons
                 message = "hello?";
                 break;
         }
-        pTftLcd -> fillScreen(TFT_TOKINEZU);
-        pTftLcd -> drawString(message, 85, 110);
-        pTftLcd -> drawString("C", 10, 2);
-        pTftLcd -> drawString("Push C to back.", 25, 205);
+        (this -> pTft) -> fillScreen(TFT_TOKINEZU);
+        (this -> pTft) -> drawString(message, 85, 110);
+        (this -> pTft) -> drawString("C", 10, 2);
+        (this -> pTft) -> drawString("Push C to back.", 25, 205);
         this -> privateFChangeDisplayState(newState);
     }
 }
@@ -53,26 +54,26 @@ void Displayer::privateFChangeDisplayState(const DisplayState newState) {
         this -> currentDisplayState = newState;
 }
 
-void Displayer::mFChangeDisplay(const Wio3Button* const pButtonInput, TFT_eSPI* const pTftLcd) {
+void Displayer::mFChangeDisplay(const Wio3Button* const pButtonInput) {
     switch (*pButtonInput) {
         case BUTTON_A:
             /* トップ画面ならA画面に遷移する */
             if (this -> currentDisplayState == MAIN_SCREEN) {
-                this -> privateFChangeScreen(DISPLAY_A, pTftLcd);
+                this -> privateFChangeScreen(DISPLAY_A);
             }
             break;
 
         case BUTTON_B:
             /* トップ画面ならB画面に遷移する */
             if (this -> currentDisplayState == MAIN_SCREEN) {
-                this -> privateFChangeScreen(DISPLAY_B, pTftLcd);
+                this -> privateFChangeScreen(DISPLAY_B);
             }
             break;
 
         case BUTTON_C:
             /* トップ画面でないならトップ画面に戻る */
             if (this -> currentDisplayState != MAIN_SCREEN) {
-                this -> privateFShowMainScreen(pTftLcd);
+                this -> privateFShowMainScreen();
             }
             break;
 
